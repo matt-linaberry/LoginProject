2434b333395e6980e23cfa3f64bd8412eebda139
Matt Linaberry <mtlinaberry@gmail.com>
2015-04-05 20:10:09 -0400

    Initial Commit

---
+++ LoginProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginProject
+//
+//  Created by Matt Linaberry on 4/5/15.
+//  Copyright (c) 2015 Matt Linaberry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
